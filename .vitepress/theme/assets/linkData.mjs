60a2e018b9097b52e0317a6e9dffd3f38d8878ed
kazukokawagawa <2580099704@qq.com>
2025-04-30 10:29:13 +0800

    modified:   .vitepress/theme/assets/linkData.mjs 	new file:   public/images/friend/5had0w.png

--- .vitepress/theme/assets/linkData.mjs
+++ .vitepress/theme/assets/linkData.mjs
@@ -21,16 +21,16 @@ const linkData = [
     typeDesc: "我们在一起，共同进步",
     typeList: [
       {
-        name: "池鱼小栈",
-        avatar: "/images/logo/logo.webp",
-        desc: "｜д•´)!!",
-        url: "https://chiyu.it",
-      },
-      {
         name: "1zkn",
         avatar: "/images/friend/zkn.jpeg",
         desc: "在技术与游戏世界中不断探索的小小冒险家",
         url: "https://zkn.moe",
+      },
+      {
+        name: "5had0w",
+        avatar: "/images/friend/5had0w.png",
+        desc: "一个游走在互联网边缘的影子，希望在这个世界留下一点痕迹。",
+        url: "https://www.5had0w.com/",
       },
     ],
   },
