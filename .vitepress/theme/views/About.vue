b67b7c014c28a78d591b99b1678ac3c296d99af1
kazukokawagawa <2580099704@qq.com>
2025-06-12 23:38:13 +0800

    更新编译时间

--- .vitepress/theme/views/About.vue
+++ .vitepress/theme/views/About.vue
@@ -186,7 +186,7 @@
           感谢你<strong>感谢我</strong>。感谢世界
         </p>
         <p class="text">
-          <strong>最后编译日期：2025.06.12</strong>--14:19
+          <strong>最后编译日期：2025.06.12</strong>--23:37
         </p>
       </div>
     </div>
